8eb863a2fc410eaff7660993fb4c516bbe21beaa
Imad Mehmood <137454357+ImadMehmood535@users.noreply.github.com>
2024-03-07 18:58:55 +0500

    push

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 import logo from "./images/logo.svg"
 import lines from "./images/lines.svg"
 import homeBanner from "./images/homeBanner.png"
-import mobilehomeBanner from "./images/mobilehomeBanner.png"
+import mobilehomeBanner from "./images/mobileHomeBanner.png"
 
 export {
     logo,
